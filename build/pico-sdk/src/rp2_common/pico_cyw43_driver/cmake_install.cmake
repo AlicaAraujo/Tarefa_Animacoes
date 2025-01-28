be1bc11a9a66312d72f10b422affe84b1af2375b
Alícia Araújo <aliciaoliara@gmail.com>
2025-01-28 19:23:56 -0300

    Alteração do código base e inserção da animação 2

--- build/pico-sdk/src/rp2_common/pico_cyw43_driver/cmake_install.cmake
+++ build/pico-sdk/src/rp2_common/pico_cyw43_driver/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/esantos/.pico-sdk/sdk/2.1.0/src/rp2_common/pico_cyw43_driver
+# Install script for directory: C:/Users/alici/.pico-sdk/sdk/2.1.0/src/rp2_common/pico_cyw43_driver
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/animacao_MatrizLED")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,11 +34,11 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/home/esantos/.pico-sdk/toolchain/13_3_Rel1/bin/arm-none-eabi-objdump")
+  set(CMAKE_OBJDUMP "C:/Users/alici/.pico-sdk/toolchain/13_3_Rel1/bin/arm-none-eabi-objdump.exe")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/esantos/Projects/VSCode_pico/Tarefa_animacoes/Tarefa_Animacoes/build/pico-sdk/src/rp2_common/pico_cyw43_driver/cybt_shared_bus/cmake_install.cmake")
+  include("C:/Users/alici/Tarefa_Animacoes/Tarefa_Animacoes/build/pico-sdk/src/rp2_common/pico_cyw43_driver/cybt_shared_bus/cmake_install.cmake")
 endif()
 
